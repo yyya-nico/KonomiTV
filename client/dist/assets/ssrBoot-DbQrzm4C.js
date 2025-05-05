b9bcf0d3eb2e8b2b9d0fc973f75599aeb5dfa2a2
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2025-05-06 02:22:08 +0900

    Client ビルドを更新

---
+++ client/dist/assets/ssrBoot-DbQrzm4C.js
@@ -0,0 +1,1 @@
+import{c as e}from"./logo-CHVboZgx.js";import{ak as t,M as s,ah as a,av as n}from"./SnackbarsStore-Buj_rJWA.js";const c=e("v-spacer","div","VSpacer");function l(){const o=t(!1);return s(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:a(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{c as V,l as u};
