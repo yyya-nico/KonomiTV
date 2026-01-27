11e0b41156d89c0e6ba740c61198ff8f3dfb328c
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2026-01-28 01:53:25 +0900

    Revert "Update: [Client] ビルドを更新"
    This reverts commit 4cf7ea927789fba631bfff79def898a07c9a754c.

---
+++ client/dist/assets/ssrBoot-CmxTMY-o.js
@@ -0,0 +1,1 @@
+import{c as e}from"./logo-CvNxcGdY.js";import{al as t,M as s,ai as a,aw as n}from"./SnackbarsStore-IT_VMLII.js";const c=e("v-spacer","div","VSpacer");function l(){const o=t(!1);return s(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:a(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{c as V,l as u};
